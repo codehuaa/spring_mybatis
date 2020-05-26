354f7415fafd68ef6d62d3b50e6319f0535cba07
华华华华 <1055928999@qq.com>
2020-05-26 16:36:36 +0800

    first submit

---
+++ src/main/java/com/wzh/mapper/UserMapperImpl.java
@@ -0,0 +1,20 @@
+package com.wzh.mapper;
+
+import com.wzh.pojo.User;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+
+import java.util.List;
+
+public class UserMapperImpl implements UserMapper {
+    private SqlSessionTemplate sqlSession;
+
+    public void setSqlSession(SqlSessionTemplate sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public List<User> getAllUsers() {
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        return mapper.getAllUsers();
+    }
+}
